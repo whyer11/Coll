b5ca76f54d5dceb6b93920f6fb063ebaac1b4ac5
辰酉 <huanyu.why@alibaba-inc.com>
2014-12-31 17:40:54 +0800

    you can pretend that it was me

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
 /**
  * Created by wanghuanyu on 14-12-31.
  */
-var request = require('request');
-var iconv = require('iconv-lite');
-var env = require('jsdom').env;
 var util = require('./util');
 
-request('http://www.zhihu.com/question/27220036', function (err, res, body) {
-    //console.log(body);
-    env(body, function (err, window) {
-        //console.log(err);
-        var $ = require('jquery')(window);
-        var str = $('.zm-item-title').text();
-        var ab = util.clearReturn(str);
-        console.log(ab);
-    })
+util.wget('https://kyfw.12306.cn/otn/regist/init', function ($,res,body) {
+    console.log(body);
 });
-
-
-//console.log($);
-
-
-//env(html, function (err, window) {
-//    var $ = require('jquery')(window);
-//    console.log($('#aa').text());
-//})
--- util.js
+++ util.js
@@ -1,8 +1,21 @@
 /**
  * Created by wanghuanyu on 14-12-31.
  */
+
+var request = require('request');
+var env = require('jsdom').env;
 module.exports = {
     clearReturn: function (str) {
         return str.replace(/\n/gm, '');
+    },
+    wget: function (url,cb) {
+        request(url, function (err, res, body) {
+            if(err) throw err;
+            env(body, function (err, window) {
+                if(err) throw err;
+                var $ = require('jquery')(window);
+                return cb($,res,body);
+            })
+        })
     }
 }
