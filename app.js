67ed39b7fcab612f67b96c9a62e46af6c209964a
辰酉 <huanyu.why@alibaba-inc.com>
2015-03-28 11:21:49 +0800

    代码没动，居然跑不起来了，还是用别人的框架先

--- app.js
+++ app.js
@@ -2,86 +2,132 @@
  * Created by wanghuanyu on 14-12-31.
  */
 var util = require('./util');
-var mysql = require('mysql');
-//var _mysql = require('./mysql');
+var orm = require('orm');
 var url = 'http://www.zhihu.com/question/';
-//var tempCount = 1;
-//var timeCount = 0;
-/**
- * the first question of zhihu
- * @type {number}
- */
+
 var questionId = 19550225;
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: ''
-});
+var config = {
+    protocol: "mysql",
+    host: "127.0.0.1",
+    database: "zhihuo",
+    port:'3306',
+    query:{ pool: true },
+    user:"root",
+    password:""
+}
 
+orm.connect(config, function (err, db) {
+    var question = db.define("question",{
+        id:Number,
+        QUES_ID:Number,
+        QUES_TITLE:String,
+        ANSW_BEST:String,
+        STATUS:Number
+    },{
+        id:'id'
+    });
+    function something(qid){
+        var currentQuestionUrl = url+qid;
+        util.wget(currentQuestionUrl, function ($, res, body) {
+            var status = res.statusCode;
 
-//var tr = new util.TimeRecord();
-
-var coll = function (id) {
-    //tr.begin();
-    //console.log(start.getTime());
-    //var a = new Date();
-    //var start = a.getTime();
-
-    var currentQuestionUrl = url+id;
-    util.wget(currentQuestionUrl, function ($, res, body) {
-        var status = res.statusCode;
-        if(body.substring(0,2) != '<!'){
-            //console.log(id+' : '+tr.end());
-            return coll(id);
-        }else {
-            var title = util.clearReturn($(body).find('.zm-item-title').text());
-            var topAnswer = $(body).find('#zh-question-answer-wrap').children().first();
-            var voteCount = util.clearReturn(topAnswer.find('.zm-votebar .up .count').text());
-            connection.query('INSERT INTO `zhihu`.`QANDA` (`QUES_ID`, `QUES_TITLE`, `QUES_STATUS`, `TOP_ANSER_COUNT`) VALUES ("'+id+'", "'+title+'", "'+status+'", "'+voteCount+'")', function (err, rows, fields) {
-                util.errHandle(err);
-                //var b = new Date();
-                //var end = b.getTime();
-                //timeCount += (end-start);
-                //
-                //var avgTime = timeCount/tempCount;
-                //tempCount++;
-                console.error(id+'---->'+status);
-                //a=null;
-                //start=null;
-                status=null;
-                title=null;
-                topAnswer=null;
-                voteCount=null;
-                //b=null;
-                //end=null;
-                //avgTime=null;
-                return coll(++id);
-
-            });
-
-
-        }
-    })
-};
-
-
-
-connection.connect();
-connection.query('SELECT max(zhihu.QANDA.QUES_ID) FROM zhihu.QANDA', function (err,rows,field) {
-    util.errHandle(err);
-    console.log('The last question is :'+rows[0]['max(zhihu.QANDA.QUES_ID)']);
-    if(rows[0]['max(zhihu.QANDA.QUES_ID)']){
-        var id = parseInt(rows[0]['max(zhihu.QANDA.QUES_ID)'])+1;
-        coll(id);
-    }else{
-        coll(questionId);
+            if(body.substring(0,2) != '<!'){
+                return something(qid);
+            }else if(status == '200'){
+                var title = $(body).find('.zm-item-title').text();
+                var topAnswer = $(body).find('#zh-question-answer-wrap').children().first();
+                var voteCount = topAnswer.find('.zm-votebar .up .count').text();
+                question.create({
+                    QUES_ID:qid,
+                    QUES_TITLE:title,
+                    ANSW_BEST:voteCount,
+                    STATUS:status
+                },function(err){
+                    if(err) console.log(err);
+                });
+                console.log(qid+'--'+status+'---'+(process.memoryUsage().heapUsed/process.memoryUsage().heapTotal)*100);
+                something(++qid);
+            }else{
+                question.create({
+                    QUES_ID:qid,
+                    QUES_TITLE:null,
+                    ANSW_BEST:null,
+                    STATUS:status
+                },function(err){
+                    if(err) console.log(err);
+                });
+                console.log(qid+'--'+status+'---'+(process.memoryUsage().heapUsed/process.memoryUsage().heapTotal)*100);
+                something(++qid);
+            }
+        })
     }
-});
+    db.driver.execQuery('select max(QUES_ID) as max from question', function (err,data) {
+        console.log('已找到最大问题ID为'+data[0].max);
+        if(data[0].max){
+            var max = parseInt(data[0].max)+1;
+            something(max);
+        }else{
+            something(questionId);
+        }
+    });
 
-//
-//_mysql.query('SELECT * FROM paw.test', function (err, rows, fields) {
-//    if(err) throw err;
-//    console.log(rows);
-//    console.log(fields);
-//})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
