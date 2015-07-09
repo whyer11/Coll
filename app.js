9fdc30f0fa9862c0efaaefcabf000a4c18558860
huanyu.wang <why490078184@gmail.com>
2015-07-09 12:58:56 +0800

    增加文档

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 /**
  * Created by wanghuanyu on 14-12-31.
  */
-var util = require('./util');
-var orm = require('orm');
-var url = 'http://www.zhihu.com/question/';
-var config = require('./config');
+var util       = require ('./util');
+var orm        = require ('orm');
+var url        = 'http://www.zhihu.com/question/';
+var config     = require ('./config');
 var questionId = 19550225;
-orm.connect(config, function (err, db) {
-    var question = db.define("question", {
+orm.connect (config, function (err, db) {
+    var question = db.define ("question", {
         id: Number,
         QUES_ID: Number,
         QUES_TOP: Number,
@@ -16,106 +16,48 @@ orm.connect(config, function (err, db) {
     }, {
         id: 'id'
     });
+    db.sync(function (err) {
+        if(err) console.log (err);
+    })
 
-    function something(qid) {
+    function something (qid) {
         var currentQuestionUrl = url + qid;
-        util.wget(currentQuestionUrl, function ($, res, body) {
-            var title = util.clearReturn($(body).find('.zm-item-title').text());
-            var toper = parseInt($(body).find('.zm-item-vote-info').data('votecount'));
+        util.wget (currentQuestionUrl, function ($, res, body) {
+            var title = util.clearReturn ($ (body).find ('.zm-item-title').text ());
+            var toper = parseInt ($ (body).find ('.zm-item-vote-info').data ('votecount'));
             if (res.statusCode == '200') {
-                //console.log(qid+'----'+toper);
-                //something(++qid);
-                question.create({
+                question.create ({
                     QUES_ID: qid,
                     QUES_TOP: toper || null,
                     QUES_TITLE: title,
                     STATUS: res.statusCode
                 }, function (err) {
-                    if (err) console.log(err);
-                    console.log(qid + '--' + res.statusCode + '---' + (process.memoryUsage().heapUsed / process.memoryUsage().heapTotal) * 100);
-                    something(++qid);
+                    if (err) console.log (err);
+                    console.log (qid + '--' + res.statusCode + '---' + (process.memoryUsage ().heapUsed / process.memoryUsage ().heapTotal) * 100);
+                    something (++qid);
                 });
             } else {
-                question.create({
+                question.create ({
                     QUES_ID: qid,
                     QUES_TOP: null,
                     QUES_TITLE: null,
                     STATUS: res.statusCode
                 }, function (err) {
-                    if (err) console.log(err);
-                    console.log(qid + '--' + res.statusCode + '---' + (process.memoryUsage().heapUsed / process.memoryUsage().heapTotal) * 100);
-                    something(++qid);
+                    if (err) console.log (err);
+                    console.log (qid + '--' + res.statusCode + '---' + (process.memoryUsage ().heapUsed / process.memoryUsage ().heapTotal) * 100);
+                    something (++qid);
                 });
             }
         })
     }
 
-    db.driver.execQuery('select max(QUES_ID) as max from question', function (err, data) {
-        console.log('已找到最大问题ID为' + data[0].max);
+    db.driver.execQuery ('select max(QUES_ID) as max from question', function (err, data) {
+        console.log ('已找到最大问题ID为' + data[0].max);
         if (data[0].max) {
-            something(parseInt(data[0].max) + 1);
-
+            something (parseInt (data[0].max) + 1);
         } else {
-            something(questionId);
+            something (questionId);
         }
     });
     //something(23684594);
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
